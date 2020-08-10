100443359a1f00fc06a3889b7643dd99314758a8
Erik <kimseyerik@gmail.com>
2020-08-10 13:45:20 -0400

    Created KD_Node script for node object.  Began building kd tree functionality

---
+++ Assets/Scripts/KD_Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KD_Node : MonoBehaviour
+{
+    private int nodeId;
+    private Vector3 nodePosition;
+    private bool isRoot;
+
+    public void SetNodeID(int _ID){
+        this.nodeId = _ID;
+    }
+
+    public void SetNodePosition(Vector3 _nodePosition){
+        this.nodePosition = _nodePosition;
+    }
+
+    public Vector3 GetNodePosition(){
+        return this.nodePosition;
+    }
+}
---
+++ Assets/Scripts/KD_Tree.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class KD_Tree : MonoBehaviour
+{
+
+    GameObject[] treeNodesArr;
+    List<KD_Node> treeNodesList;
+    public string tagName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetTreeNodesArray();
+        treeNodesList = new List<KD_Node>();
+    }
+
+    private void SetTreeNodesArray(){
+        treeNodesArr = GameObject.FindGameObjectsWithTag(tagName);
+        Debug.Log(treeNodesArr[0].transform.position);
+    }
+
+    private void CreateKDTree(){
+        foreach (var item in treeNodesArr)
+        {
+            
+        }
+    }
+
+    private void InsertNode(GameObject ){
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
