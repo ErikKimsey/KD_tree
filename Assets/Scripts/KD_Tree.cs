e55ab7f47f7d110055375db2bbf06f5b573dd502
Erik <kimseyerik@gmail.com>
2020-08-11 10:11:52 -0400

    Working out func for sorted insert

--- Assets/Scripts/KD_Tree.cs
+++ Assets/Scripts/KD_Tree.cs
@@ -11,18 +11,19 @@ public class KD_Tree : MonoBehaviour
     List<KD_Node> treeNodesList;
     public string tagName;
     private int totalNodeCount;
-    // Start is called before the first frame update
+    private int nodeListDepth;
+
     public float xAngle, yAngle, zAngle;
     void Start()
     {
         StartCoroutine(SetTreeNodesArray());
         treeNodesList = new List<KD_Node>();
+        nodeListDepth = 0;
     }
 
     IEnumerator SetTreeNodesArray(){
         treeNodesArr = GameObject.FindGameObjectsWithTag(tagName);
         totalNodeCount = treeNodesArr.Length;
-        // Debug.Log(totalNodeCount);
         if(totalNodeCount > 0){
             for (int i = 0; i < totalNodeCount; i++)
             {
@@ -37,13 +38,34 @@ public class KD_Tree : MonoBehaviour
             KD_Node root = new KD_Node(treeNodesArr[0], 0);
             root.SetIsRoot(true);
             treeNodesList.Add(root);
+            nodeListDepth += 1;
         } else {
             for (int i = 1; i < totalNodeCount; i++)
             {
-                
+                if(i % 3 == 0){
+
+                } else if (i % 2 == 0){
+                    
+                } else {
+                    // if (i-1 != root):
+                        // if(arr[i] > arr[i-1]):
+                            // arr[i] == arr[i-1].rightChild
+                        // else:
+                            // arr[i] == arr[i-1].leftChild
+                }
+
             }
         }
+
+        // if not root (dim 0):
+        // -- if (dim % 3 == 0): compare Z
+        //  -- if (dim % 2 == 0): compare Y
+        //  -- else: compare X
     }
+
+    private 
+
+
 
     private void InsertNode(GameObject _nodeObj){
 
