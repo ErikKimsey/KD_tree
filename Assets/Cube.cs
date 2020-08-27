09377ac7a0c446b9d5c95ef0a92a28e58cf8a4b5
Erik Kimsey <kimseyerik@gmail.com>
2020-08-27 15:25:30 -0400

    Have Cube of pyramids generating dynamically.  Added proper material to pyramid mesh

--- Assets/Cube.cs
+++ Assets/Cube.cs
@@ -6,30 +6,41 @@ public class Cube : MonoBehaviour
 {
     public GameObject pyramid;
     public Vector3 startPosition;
-    public int totalPyramids;
+    public int dimensionCount;
+    private float xAx, yAx, zAx;
+    private Vector3 currPos;
+    public float distanceMultiplier;
+    public Quaternion pyramidRotation;
+    private float startX, startY, startZ;
     
 
     void Start()
     {
+        startX = startPosition.x;
+        Debug.Log(startX);
+        startY = startPosition.y;
+        Debug.Log(startY);
+        startZ = startPosition.z;
+        Debug.Log(startZ);
         BuildCube();
     }
 
     private void BuildCube(){
-        Vector3 currPos = startPosition;
-        for (int i = 0; i < totalPyramids; i++)
-        {
-            // if(currPos == null){
-                currPos = startPosition;
-                GameObject clone = Instantiate(pyramid, currPos, Quaternion.identity);
-            // } else {
-                // Vector3 temp = DeterminePosition(i, );
-            // }
+       
+        for (int x = 0; x < dimensionCount; x++){
+            for (int y = 0; y < dimensionCount; y++){
+                for (int z = 0; z < dimensionCount; z++){
+                    xAx =  startX + x * distanceMultiplier;
+                    yAx = startY + y * distanceMultiplier;
+                    zAx = startZ + z * distanceMultiplier;
+                    currPos = new Vector3(xAx, yAx, zAx);
+                    Debug.Log(currPos);
+                    GameObject clone = Instantiate(pyramid, currPos, pyramidRotation);
+                    clone.transform.SetParent(transform);
+                }
+            }
         }
     }
-
-    // private Vector3 DeterminePosition(int _index, Vector3 currPos){
-        // Vector3 temp = n
-    // }
 
     // Update is called once per frame
     void Update()
